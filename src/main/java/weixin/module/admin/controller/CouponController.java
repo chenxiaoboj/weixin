51cd93495e8c9d95e244fb63726160c97b5313cb
chenx <chenxiaobo@igeshui.com>
2019-01-07 15:03:08 +0800

    test

--- src/main/java/weixin/module/admin/controller/CouponController.java
+++ src/main/java/weixin/module/admin/controller/CouponController.java
@@ -1,4 +1,10 @@
 package weixin.module.admin.controller;
 
+import io.swagger.annotations.Api;
+
+/**
+ * @author chenx
+ */
+@Api(value ="CouponController",tags = "优惠卷")
 public class CouponController {
 }
